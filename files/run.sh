d02d37a4a444e686f20cd8d6943d04d0b87d0b74
Andrew Gracey <andrew.gracey@suse.com>
2022-09-04 19:56:04 -0700

    Create run.sh

---
+++ files/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir -p /var/lib/rancher/k3s/agent/etc/containerd/
+cp config.toml.tmpl /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl
+
+cp containerd-shim-spin-v1 /usr/local/bin/containerd-shim-spin-v1
+chmod a+x /usr/local/bin/containerd-shim-spin-v1
