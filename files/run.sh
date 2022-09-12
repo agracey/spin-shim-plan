a732ef4c364cf2a5b6e5ded522060debe3f428ef
Andrew Gracey <andrew.gracey@suse.com>
2022-09-11 19:33:45 -0700

    adding wasi.yaml

--- files/run.sh
+++ files/run.sh
@@ -7,3 +7,7 @@ cp config.toml.tmpl /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl
 
 cp containerd-shim-spin-v1 /usr/local/bin/containerd-shim-spin-v1
 chmod a+x /usr/local/bin/containerd-shim-spin-v1
+
+
+mkdir -p /var/lib/rancher/k3s/server/manifests 
+cp wasi.yaml /var/lib/rancher/k3s/server/manifests/
