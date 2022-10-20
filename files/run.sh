d0f6a071e315b977ecfcba17c533cdcdd6fff687
Andrew Gracey <andrew.gracey@suse.com>
2022-10-20 12:20:18 -0700

    updating and adding slight

--- files/run.sh
+++ files/run.sh
@@ -7,6 +7,8 @@ cp config.toml.tmpl /var/lib/rancher/k3s/agent/etc/containerd/config.toml.tmpl
 
 cp containerd-shim-spin-v1 /usr/local/bin/containerd-shim-spin-v1
 chmod a+x /usr/local/bin/containerd-shim-spin-v1
+cp containerd-shim-spin-v1 /usr/local/bin/containerd-shim-slight-v1
+chmod a+x /usr/local/bin/containerd-shim-slight-v1
 
 
 mkdir -p /var/lib/rancher/k3s/server/manifests 
